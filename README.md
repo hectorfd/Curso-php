c9c00c03c0bce104f7252b53244af9ce764b324a
61565698+hectorfd@users.noreply.github.com <61565698+hectorfd@users.noreply.github.com>
2024-01-09 22:54:55 -0500

    hola mundo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Curso de PHP
 aquí almacenaré mi aprendizaje para es curso de PHP de la Universidad
 aquí habrá una lista de lo que aprenderemos dentro del curso
-* hola mundo  
+* Hola Mundo  
---
+++ php/1-hola-mundo.php
@@ -0,0 +1,25 @@
+<?php
+echo "Hola mundo \n";
+
+echo "Este es mi programa";
+
+$mi_texto = "esto es un texto no te olvides de cerrar el punto y coma";
+echo $mi_texto . "\n";
+echo gettype($mi_texto);// imprime el tipo de dato como contiene texto esto es un string
+
+$mi_texto = 6;
+echo "\n". $mi_texto. "\n";
+echo gettype($mi_texto);// el tipo de dato cambia a integer 
+
+$entero = 7;
+$entero = $entero +4;
+echo $entero ."\n";
+echo $entero -1 ."\n";
+
+//para decimales utilizaremos double
+$my_double = 8.3;
+echo gettype($my_double)."\n";
+// php es un lenguaje de tipado dinámico 
+$my_bool = true;
+echo $my_bool."\n";
+echo gettype($my_bool);
