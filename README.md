a35cd960c71063467d0a822afc465a33cbf1e36a
61565698+hectorfd@users.noreply.github.com <61565698+hectorfd@users.noreply.github.com>
2024-01-11 10:30:46 -0500

    intro

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Curso de PHP
 aquí almacenaré mi aprendizaje para es curso de PHP de la Universidad
-aquí habrá una lista de lo que aprenderemos dentro del curso
-* Hola Mundo 
+aquí habrá una lista de lo que aprenderemos dentro del curso 
 * ## PHP
 Creado por Rasmus Lerdorf en 1994, PHP significa **Hypertext Pre-Processor**, es decir, pre-procesador de hipertexto.
 Se utiliza en el backend para realizar paginas dinámicas
--- class/01-intro.php
+++ class/01-intro.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
     <title>PHP intro</title>
 </head>
 <body>
@@ -12,11 +13,46 @@
     <section>
         <h2>Fundamentos de Programación</h2>
         <?php
-        echo "hola mundo";
+        echo "1. Integrar código PHP en HTML .<br>";
+        echo "2. Pagina estática vs Pagina dinámica .<br>";
+        echo "3. Manejo de variables en PHP .<br>";
+        ?>
+    </section>
+    <br>
+    <section>
+        <h2>Sueldos</h2>
+        <?php
+        $sueldo = 350;
+        printf("El sueldo básico es: %.2f",$sueldo);
+        
+        ?>
+    </section>
+    <br>
+    <section>
+        <h2>LISTADO DE CASOS DESARROLLADOS CON PHP</h2>
+        <?php
+        $cadena = "CASO";
+        echo '<ul>';
+        for ($i=1; $i < 8; $i++) { 
+            echo('<li>'.$cadena." ". $i.'</li>');
+        }
+        echo '</ul>';
+        ?>
+    </section>
+    <br>
+    <section>
+        <h2>variable literal</h2>
+        <?php
+            //!Literales
+            //Entero -> 10 100 230 -123
+            //Reales -> 20.50 0.0034 -123.5
+            //boolean -> true false
+            //carácter -> "a"
+            //cadena -> "Hector"
         ?>
     </section>
     <footer>
-        <h3>todos los derehos reservados</h3>
+        <h6>Copyright © 2024, todos los derechos reservados </h6>
     </footer>
     
 </body>
