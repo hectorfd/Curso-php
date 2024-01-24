129e8334dc9cebf4099b553cae21f04f52cbafa9
61565698+hectorfd@users.noreply.github.com <61565698+hectorfd@users.noreply.github.com>
2024-01-24 10:23:12 -0500

    caso 8 listo para programar

--- Casos/caso8.php
+++ Casos/caso8.php
@@ -36,33 +36,27 @@
                  error_reporting(0);
                 
 
-                 $alumno = $_GET['txtAlumno'];
-                 $categoria = $_GET['txtLista'];
-                 $promedio = $_GET['txtPromedio'];
-
-
-
                  ?>
-                <div class="centrar"><img src="../images/estudiantes.png" alt="" width="880px"></div>
+                <div class="centrar"><img src="../images/teatro.png" alt="" width="480px"></div>
                 <tr>
                     <td>Comprador</td>
                     
-                    <td><input required type="text" name="txtAlumno" value="<?php echo $alumno; ?>" id="ancho" placeholder="Ingrese nombre del cliente">
+                    <td><input required type="text" name="txtAlumno" value="<?php echo $comprador; ?>" id="ancho" placeholder="Ingrese nombre del cliente">
                 </td>
                 
                 </tr>
                 <tr>
                     <td><label for="">Fecha Actual</label></td>
-                    <td><input required type="date" name="txtPromedio" value="<?php echo $promedio;?>" id="ancho4" placeholder="Promedio"> </td>
+                    <td><input required type="date" name="txtPromedio" value="<?php echo $fecha;?>" id="ancho4" placeholder="Promedio"> </td>
                    
                 </tr>
                 <tr>
                     <td><label for="">N entradas adultos</label></td>
-                    <td><input required type="number" name="txtPromedio" value="<?php echo $promedio;?>" id="ancho4" placeholder="Promedio"> </td>
+                    <td><input required type="number" name="txtPromedio" value="<?php echo $adultos;?>" id="ancho4" placeholder="Ingrese entradas"> </td>
                 </tr>
                 <tr>
                     <td><label for="">N entradas niños</label></td>
-                    <td><input required type="number" name="txtPromedio" value="<?php echo $promedio;?>" id="ancho4" placeholder="Promedio"> </td>
+                    <td><input required type="number" name="txtPromedio" value="<?php echo $niños;?>" id="ancho4" placeholder="ingrese entradas"> </td>
                 </tr>
                 
                 <tr>
