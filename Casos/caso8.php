25afb1890f3f184a1693c9c11c2e5c0e11d03404
61565698+hectorfd@users.noreply.github.com <61565698+hectorfd@users.noreply.github.com>
2024-01-24 10:15:11 -0500

    caso 8 esqueleto

---
+++ Casos/caso8.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../html/css/style.css">
+    <title>Caso 8</title>
+    
+</head>
+<body>
+    <header>
+        <h1>Caso 8</h1>
+        <div class="wrapper">
+            <div class="select-btn">
+                <span>Selecciona Caso</span>
+                <i class="uil uil-angle-down"></i>
+            </div>
+            <div class="content">
+                <div class="search">
+                    <i class="uil uil-search"></i>
+                    <input type="text" spellcheck="false" placeholder="Buscar">
+                </div>
+                <ul class="options"></ul>
+            </div>
+        </div>
+    </header>
+    <nav>
+        
+    </nav>
+    <section>
+        <h2>Venta de entradas (Teatro)</h2>
+        <form action="" >
+            <table border="0" cellspacing ="0" cellpadding ="0" align="center">
+                <?php
+                 error_reporting(0);
+                
+
+                 $alumno = $_GET['txtAlumno'];
+                 $categoria = $_GET['txtLista'];
+                 $promedio = $_GET['txtPromedio'];
+
+
+
+                 ?>
+                <div class="centrar"><img src="../images/estudiantes.png" alt="" width="880px"></div>
+                <tr>
+                    <td>Comprador</td>
+                    
+                    <td><input required type="text" name="txtAlumno" value="<?php echo $alumno; ?>" id="ancho" placeholder="Ingrese nombre del cliente">
+                </td>
+                
+                </tr>
+                <tr>
+                    <td><label for="">Fecha Actual</label></td>
+                    <td><input required type="date" name="txtPromedio" value="<?php echo $promedio;?>" id="ancho4" placeholder="Promedio"> </td>
+                   
+                </tr>
+                <tr>
+                    <td><label for="">N entradas adultos</label></td>
+                    <td><input required type="number" name="txtPromedio" value="<?php echo $promedio;?>" id="ancho4" placeholder="Promedio"> </td>
+                </tr>
+                <tr>
+                    <td><label for="">N entradas niños</label></td>
+                    <td><input required type="number" name="txtPromedio" value="<?php echo $promedio;?>" id="ancho4" placeholder="Promedio"> </td>
+                </tr>
+                
+                <tr>
+                    <td></td>
+                    <td><input type="submit" value="Procesar" id="button"/></td></td>
+                </tr>
+                
+                
+            </table>      
+        </form> 
+    </section>
+    <br>
+    <footer>
+        <h6>HectorFD Copyright © 2024, todos los derechos reservados </h6>
+    </footer>
+    <script src="../html/js/script.js"></script>
+</body>
+</html>
